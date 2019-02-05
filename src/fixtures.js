0ca8b115d16fa97113deed1508dee38bf5fc9063
kovalev <smolensk123>
2019-02-05 18:23:20 +0300

    первый урок

---
+++ src/fixtures.js
@@ -0,0 +1,12 @@
+export const articles = [
+    {
+        "id": 123,
+        "title": 'title123',
+        "text": 'text123'
+    },
+    {
+        "id": 234,
+        "title": 'title234',
+        "text": 'text234'
+    },
+];
